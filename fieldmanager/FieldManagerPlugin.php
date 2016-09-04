12962a20761a5945df04e92421085119509105a9
Josh Crawford <joshua@sgroup.com.au>
2016-09-05 07:40:39 +1000

    version 1.5.3

--- fieldmanager/FieldManagerPlugin.php
+++ fieldmanager/FieldManagerPlugin.php
@@ -14,7 +14,7 @@ class FieldManagerPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.5.2';
+        return '1.5.3';
     }
 
     public function getSchemaVersion()

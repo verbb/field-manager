d3d13af80a945eaf4f955c4f9c94ce35152a61ed
Josh Crawford <Josh@engramdesign.com.au>
2017-11-05 16:34:07 +1100

    version 1.5.5

--- fieldmanager/FieldManagerPlugin.php
+++ fieldmanager/FieldManagerPlugin.php
@@ -14,7 +14,7 @@ class FieldManagerPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.5.4';
+        return '1.5.5';
     }
 
     public function getSchemaVersion()

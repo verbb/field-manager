7b40a2a01c62c699ed84e3ee3eb443c75dcea928
Josh Crawford <joshua@sgroup.com.au>
2018-10-24 18:22:19 +1100

    Clean up main plugin class

--- src/FieldManager.php
+++ src/FieldManager.php
@@ -3,9 +3,6 @@ namespace verbb\fieldmanager;
 
 use verbb\fieldmanager\base\PluginTrait;
 use verbb\fieldmanager\models\Settings;
-use verbb\fieldmanager\services\Service;
-use verbb\fieldmanager\services\Import;
-use verbb\fieldmanager\services\Export;
 
 use Craft;
 use craft\base\Plugin;
@@ -37,51 +34,39 @@ class FieldManager extends Plugin
 
         self::$plugin = $this;
 
-        // Register Components (Services)
-        $this->setComponents([
-            'service' => Service::class,
-            'import'  => Import::class,
-            'export'  => Export::class,
-        ]);
+        $this->_setPluginComponents();
+        $this->_setLogging();
+        $this->_registerCpRoutes();
 
-        // Register CP routes
-        Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_CP_URL_RULES, [$this, 'registerCpUrlRules']);
-
-        // Handle cp panel icon
-        $this->hasCpSection = $this->service->isCpSectionEnabled();
-    }
-
-    /**
-     * @param RegisterUrlRulesEvent $event
-     */
-    public function registerCpUrlRules(RegisterUrlRulesEvent $event)
-    {
-        $rules = [
-            'field-manager' => 'field-manager/base/index',
-        ];
-
-        $event->rules = array_merge($event->rules, $rules);
+        $this->hasCpSection = $this->getService()->isCpSectionEnabled();
     }
 
 
     // Protected Methods
     // =========================================================================
 
-    /**
-     * @return Settings
-     */
     protected function createSettingsModel(): Settings
     {
         return new Settings();
     }
 
-    /**
-     * @return string
-     */
     protected function settingsHtml(): string
     {
         return Craft::$app->getView()->renderTemplate('field-manager/settings', [
             'settings' => $this->getSettings(),
         ]);
+    }
+
+
+    // Private Methods
+    // =========================================================================
+
+    private function _registerCpRoutes()
+    {
+        Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_CP_URL_RULES, function(RegisterUrlRulesEvent $event) {
+            $event->rules = array_merge($event->rules, [
+                'field-manager' => 'field-manager/base/index',
+            ]);
+        });
     }
 }
--- src/base/PluginTrait.php
+++ src/base/PluginTrait.php
@@ -2,39 +2,66 @@
 namespace verbb\fieldmanager\base;
 
 use verbb\fieldmanager\FieldManager;
+use verbb\fieldmanager\services\Service;
+use verbb\fieldmanager\services\Import;
+use verbb\fieldmanager\services\Export;
 
 use Craft;
+use craft\log\FileTarget;
+
+use yii\log\Logger;
 
 trait PluginTrait
 {
     // Static Properties
     // =========================================================================
 
-    /**
-     * @var FieldManager
-     */
     public static $plugin;
-
-
-    // Static Methods
-    // =========================================================================
-
-    public static function error($message, array $params = [])
-    {
-        Craft::error(Craft::t('field-manager', $message, $params), __METHOD__);
-    }
-
-    public static function info($message, array $params = [])
-    {
-        Craft::info(Craft::t('field-manager', $message, $params), __METHOD__);
-    }
 
 
     // Public Methods
     // =========================================================================
 
+    public function getImport()
+    {
+        return $this->get('import');
+    }
+
     public function getService()
     {
         return $this->get('service');
     }
+
+    public function getExport()
+    {
+        return $this->get('export');
+    }
+
+    private function _setPluginComponents()
+    {
+        $this->setComponents([
+            'service' => Service::class,
+            'import' => Import::class,
+            'export' => Export::class,
+        ]);
+    }
+
+    private function _setLogging()
+    {
+        Craft::getLogger()->dispatcher->targets[] = new FileTarget([
+            'logFile' => Craft::getAlias('@storage/logs/field-manager.log'),
+            'categories' => ['field-manager'],
+        ]);
+    }
+
+    public static function log($message)
+    {
+        Craft::getLogger()->log($message, Logger::LEVEL_INFO, 'field-manager');
+    }
+
+    public static function error($message)
+    {
+        Craft::getLogger()->log($message, Logger::LEVEL_ERROR, 'field-manager');
+    }
+
 }
